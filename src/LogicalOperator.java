0436516f39a2460dd17aa6a30ffe6ce309eafa5c
LachTomasz <lach.tomasz@gmail.com>
2022-03-14 22:45:59 +0100

    new version

--- src/LogicalOperator.java
+++ src/LogicalOperator.java
@@ -2,13 +2,30 @@ public class LogicalOperator {
     public static void main(String[] args) {
         var logical = new boolean[]{false, true};
         System.out.println();
-        for(int i = 0; i < logical.length; i++)
-            for(int j = 0; j < logical.length; j++)
-                if (logical[i] & logical[j])
-                    System.out.println(logical[i] + " AND " + logical[j] + " => true");
-                else if(logical[i] | logical[j])
-                    System.out.println(logical[i] + " AND " + logical[j] + " => false");
-                else if(!logical[i])
-                    System.out.println("NOT " + logical[i] + " => true");
+        System.out.println("=======================================================");
+        System.out.println("Negacje");
+        System.out.println("=======================================================");
+        System.out.println("p\t\t!p");
+        for (boolean p : logical) {
+            System.out.println(p + "\t=>\t"+!p);
+        }
+        System.out.println();
+        System.out.println("=======================================================");
+        System.out.println("p\t&\tq");
+        System.out.println("=======================================================");
+        for (boolean p: logical) {
+            for (boolean q: logical) {
+                System.out.println(p + "\t&\t" + q + "\t=>\t" + (p&q));
+            }
+        }
+        System.out.println();
+        System.out.println("=======================================================");
+        System.out.println("p\t|\tq");
+        System.out.println("=======================================================");
+        for (boolean p: logical) {
+            for (boolean q : logical) {
+                System.out.println(p + "\t|\t" + q + "\t=>\t" + (p | q));
+            }
+        }
     }
 }
