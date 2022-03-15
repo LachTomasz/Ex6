cf3c3abb3cc5893bb663c838694e1fe9b586d3ec
LachTomasz <lach.tomasz@gmail.com>
2022-03-15 20:23:32 +0100

    new version

---
+++ src/DeMorganLaws.java
@@ -0,0 +1,13 @@
+public class DeMorganLaws {
+    public static void main(String[] args) {
+        boolean[] bool = {false, true};
+        String s = "\t";
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Pierwsze prawo De Morgana - prawo zaprzeczenia koniunkcji.");
+        System.out.println("p\t\tq\t\t!p\t\t!q\t\tp&q\t\t!(p&q)\t!p|!q\t!(p&q)==(!p|!q)");
+        System.out.println("----------------------------------------------------------");
+        for (boolean p : bool)
+            for (boolean q : bool)
+                System.out.println(p+s+q+s+!p+s+!q+s+(p&q)+s+!(p&q)+s+(!p|!q)+s+(!(p&q)==(!p|!q)));
+    }
+}
