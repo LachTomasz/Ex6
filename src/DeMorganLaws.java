abd624c3ad087f6c1dba13cfc8586b3a614e9464
LachTomasz <lach.tomasz@gmail.com>
2022-03-15 20:39:02 +0100

    new version

--- src/DeMorganLaws.java
+++ src/DeMorganLaws.java
@@ -4,10 +4,19 @@ public class DeMorganLaws {
         String s = "\t";
         System.out.println("----------------------------------------------------------");
         System.out.println("Pierwsze prawo De Morgana - prawo zaprzeczenia koniunkcji.");
-        System.out.println("p\t\tq\t\t!p\t\t!q\t\tp&q\t\t!(p&q)\t!p|!q\t!(p&q)==(!p|!q)");
+        System.out.println("p\t\tq\t\tp&q\t\t!(p&q)\t!p\t\t!q\t\t!p|!q\t!(p&q)<=>(!p|!q)");
         System.out.println("----------------------------------------------------------");
         for (boolean p : bool)
             for (boolean q : bool)
-                System.out.println(p+s+q+s+!p+s+!q+s+(p&q)+s+!(p&q)+s+(!p|!q)+s+(!(p&q)==(!p|!q)));
+                System.out.println(p+s+q+s+(p&q)+s+!(p&q)+s+!p+s+!q+s+(!p|!q)+s+(!(p&q)==(!p|!q)));
+        System.out.println("==========================================================");
+        System.out.println("Drugie prawo De Morgana - prawo zaprzeczenia alternatywy.");
+        System.out.println("p\t\tq\t\tp|q\t\t!(p|q)\t!p\t\t!q\t\t!p&!q\t!(p|q)<=>(!p&!q)");
+        System.out.println("----------------------------------------------------------");
+        for(boolean p : bool)
+            for(boolean q : bool)
+                System.out.println(p+s+q+s+(p|q)+s+!(p|q)+s+!p+s+!q+s+(!p&!q)+s+(!(p|q)==(!p&!q)));
+        System.out.println("----------------------------------------------------------");
+        System.out.println();
     }
 }
