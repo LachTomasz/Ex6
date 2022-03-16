7dc60d0b86aeb7bce661f1054d345341b377765d
LachTomasz <lach.tomasz@gmail.com>
2022-03-16 23:11:18 +0100

    new version

--- src/Implikation.java
+++ src/Implikation.java
@@ -1,8 +1,11 @@
 public class Implikation {
     public static void main(String[] args) {
         boolean[] value = {false, true};
+        System.out.println();
         System.out.println("Implikacja p => q");
-        System.out.println("p\tq\tp=>q");
+        System.out.println();
+        System.out.println("p\t\tq\t\tp=>q");
+        System.out.println("--------------------");
         for (boolean p : value)
             for(boolean q : value)
                 System.out.println(p+"\t"+q+"\t"+(Boolean.valueOf(p).compareTo(q) < 1));
