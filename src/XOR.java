dfbeb0926993dc8025ce9d15ebd6bb80ca651802
LachTomasz <lach.tomasz@gmail.com>
2022-03-23 21:02:19 +0100

    and one more other solution

--- src/XOR.java
+++ src/XOR.java
@@ -2,11 +2,11 @@ public class XOR {
     public static void main(String[] args) {
         boolean[] value = {false, true};
         System.out.println();
-        System.out.println("Alternatywa wykluczająca:");
+        System.out.println("Alternatywa wykluczająca (XOR):");
         System.out.println("-------------------------");
         for(boolean p : value)
             for(boolean q: value)
-                System.out.println(p + " XOR " + q + " => " + ((p|q)&(!(p&q))));
+                System.out.println(p + "\t" + q + "\t=>\t" + ((p|q)&(!(p&q))));
         System.out.println();
         for(boolean p : value)
             for(boolean q : value)
