bbec014bcc76bae037b11e737662cdb117118cae
LachTomasz <lach.tomasz@gmail.com>
2022-03-23 20:56:53 +0100

    other solution

--- src/XOR.java
+++ src/XOR.java
@@ -8,6 +8,8 @@ public class XOR {
             for(boolean q: value)
                 System.out.println(p + " XOR " + q + " => " + ((p|q)&(!(p&q))));
         System.out.println();
-
+        for(boolean p : value)
+            for(boolean q : value)
+                System.out.println(p + "\t" + q + "\t=>\t" + (p^q) );
     }
 }
