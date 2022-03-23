62edfe74fb96aaf3af3aebc89e9d1fc33f279387
LachTomasz <lach.tomasz@gmail.com>
2022-03-23 20:54:05 +0100

    new version

---
+++ src/XOR.java
@@ -0,0 +1,13 @@
+public class XOR {
+    public static void main(String[] args) {
+        boolean[] value = {false, true};
+        System.out.println();
+        System.out.println("Alternatywa wykluczająca:");
+        System.out.println("-------------------------");
+        for(boolean p : value)
+            for(boolean q: value)
+                System.out.println(p + " XOR " + q + " => " + ((p|q)&(!(p&q))));
+        System.out.println();
+
+    }
+}
