453338191a5df458b2875b011aa89b7411724d40
LachTomasz <lach.tomasz@gmail.com>
2022-03-24 21:16:11 +0100

    New look

--- src/LawOfLogicA.java
+++ src/LawOfLogicA.java
@@ -4,16 +4,18 @@ public class LawOfLogicA {
 
         System.out.println("Prawo przechodności implikacji");
         System.out.println("p\t\tq\t\tr\t\t|\t(p => q)\t(q => r)\t|" +
-                "\t(p => q)^(q => r)\t|\t => (p => r)");
-        System.out.println("--------------------------------------" +
-                "---------------------------------------------------");
+                "\t(p => q)^(q => r)\t|\t(p => r)\t|\t[(p => q)^(q => r)]=>(p => r)");
+        System.out.println("------------------------------------------" +
+                "-----------------------------------------------------" +
+                "-------------------------------");
         for (boolean p : values)
             for(boolean q : values)
                 for (boolean r : values){
                     System.out.print(p+"\t"+q+"\t"+r+"\t|\t");
                     System.out.print(impl(p,q)+"\t\t"+impl(q,r)+"\t\t|\t\t");
-                    System.out.print((impl(p,q)&impl(q,r))+"\t\t\t|");
-                    System.out.println();
+                    System.out.print((impl(p,q)&impl(q,r))+"\t\t\t|\t");
+                    System.out.print(impl(p,r)+"\t\t|\t\t\t\t");
+                    System.out.println(impl((impl(p,q)&impl(q,r)),impl(p,r)));
                 }
     }
 
