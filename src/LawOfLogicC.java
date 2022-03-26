a87ee6394785e3eb993a482779e93693bd6960dc
LachTomasz <lach.tomasz@gmail.com>
2022-03-26 22:45:31 +0100

    Law Of Logic [p^(q v r)] <=> [(p ^ q) v (p ^ r)]

---
+++ src/LawOfLogicC.java
@@ -0,0 +1,25 @@
+public class LawOfLogicC {
+
+    /*
+    Law Of Logic [p^(q v r)] <=> [(p ^ q) v (p ^ r)]
+     */
+    public static void main(String[] args) {
+        boolean[] values = {false ,true};
+        System.out.println();
+        System.out.println("Prawo rozdzielności koniunkcji względem alternatywy");
+        System.out.println("\t[p^(q v r)] <=> [(p ^ q) v (p ^ r)]\t");
+        System.out.println();
+        System.out.println("------------------------------------" +
+                "-----------------------------------------------" +
+                "-----------------------------------------------");
+        System.out.println("p\t\tq\t\tr\t\t|\t");
+        for(boolean p : values)
+            for(boolean q : values)
+                for (boolean r : values){
+                    System.out.print(p + "\t" + q + "\t" + r + "\t|\t");
+                    System.out.print((p & q) + "\t");
+                    System.out.print(((p&q)|r) + "\t");
+                    System.out.println();
+                }
+    }
+}
