8771a9d493d3e4bb1361282dd0e3d50959d70550
LachTomasz <lach.tomasz@gmail.com>
2022-03-27 19:23:38 +0200

    New version

--- src/TestChar.java
+++ src/TestChar.java
@@ -10,8 +10,16 @@ public class TestChar {
         System.out.println("Method:\nA:\tisDigit()\nB:\tisLetter()\nC:\tisLetterOrDigit()\n" +
                 "D:\tisLowerCase()\nE:\tisSpaceChar\nF:\tisUpperCase()\nG:\tisWhitespace()P");
         TestChar(A);
+        System.out.println();
+        TestCharExample(A);
+        System.out.println();
         TestChar(B);
+        System.out.println();
+        TestCharExample(B);
+        System.out.println();
         TestChar(C);
+        System.out.println();
+        TestCharExample(C);
     }
 
     static void TestChar(String str){
@@ -30,5 +38,46 @@ public class TestChar {
             System.out.print(Character.isLowerCase(ch) + "\t" + Character.isSpaceChar(ch) + "\t");
             System.out.println(Character.isUpperCase(ch) + "\t" + Character.isWhitespace(ch));
         }
+    }
+
+    static void TestCharExample(String str){
+        System.out.println("Znaki: "+str);
+        char[] znaki = str.toCharArray();
+        System.out.print("Znak ");
+        for(char z : znaki)
+            System.out.print("\t"+z);
+        System.out.println();
+        System.out.print("Kod znaku ");
+        for(char z : znaki)
+            System.out.print("\t"+(int)z);
+        System.out.println();
+        System.out.print("isDigit() ");
+        for(char z : znaki)
+            System.out.print("\t"+Character.isDigit(z));
+        System.out.println();
+        System.out.print("isLetter() ");
+        for(char z : znaki)
+            System.out.print("\t"+Character.isLetter(z));
+        System.out.println();
+        System.out.print("isLetterOrDigit()");
+        for(char z : znaki)
+            System.out.print("\t"+Character.isLetterOrDigit(z));
+        System.out.println();
+        System.out.print("isLowerCase() ");
+        for(char z : znaki)
+            System.out.print("\t"+Character.isLowerCase(z));
+        System.out.println();
+        System.out.print("isSpaceChar() ");
+        for(char z : znaki)
+            System.out.print("\t"+Character.isSpaceChar(z));
+        System.out.println();
+        System.out.print("isUpperCase() ");
+        for(char z : znaki)
+            System.out.print("\t"+Character.isUpperCase(z));
+        System.out.println();
+        System.out.print("isWhitespace() ");
+        for(char z : znaki)
+            System.out.print("\t"+Character.isWhitespace(z));
+        System.out.println();
     }
 }
